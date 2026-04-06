e20967045f69d5a394d99d90d8f79fda0d30eecb
urielda00 <urielda00@gmail.com>
2026-04-06 18:58:33 +0300

    updated setting location

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
-  turbopack: {
-    root: "C:/Users/uriel/Desktop/portfolio",
-  },
 };
 
 export default nextConfig;
